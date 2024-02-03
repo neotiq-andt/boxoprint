1af44323788380d6e53e251ad916cd5416ceec01
andt <andt@neotiq.net>
2024-02-03 23:54:56 +0700

    check to run magento setup 1 time only

--- magento/magento-setup.sh
+++ magento/magento-setup.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 set -e
 
-# Wait for MySQL to be ready
-wait-for-it mysql:3306
+if [ ! -e /var/www/html/magento-setup-done ]; then
 
-# Run Magento setup command
-php bin/magento setup:install \
- --db-host=$MAGENTO_DB_HOST \
- --db-name=$MAGENTO_DB_NAME \
- --db-user=$MAGENTO_DB_USER \
- --db-password=$MAGENTO_DB_PASSWORD \
- --base-url=http://$MAGENTO_DOMAIN/ \
- --base-url-secure=https://$MAGENTO_DOMAIN/ \
- --admin-user=admin123 \
- --admin-password=admin123 \
- --admin-email=admin1@boxoprint.neotiq.com \
- --admin-firstname=Admin \
- --admin-lastname=User \
- --backend-frontname=admin \
- --language=fr_FR \
- --currency=EUR \
- --timezone=Europe/Paris \
- --use-rewrites=1 \
- --search-engine=elasticsearch7 --elasticsearch-host=$MAGENTO_ELASTICSEARCH_HOST --elasticsearch-port=9200
+  # Wait for MySQL to be ready
+  wait-for-it mysql:3306
+
+  # Run Magento setup command
+  php bin/magento setup:install \
+  --db-host=$MAGENTO_DB_HOST \
+  --db-name=$MAGENTO_DB_NAME \
+  --db-user=$MAGENTO_DB_USER \
+  --db-password=$MAGENTO_DB_PASSWORD \
+  --base-url=http://$MAGENTO_DOMAIN/ \
+  --base-url-secure=https://$MAGENTO_DOMAIN/ \
+  --admin-user=admin123 \
+  --admin-password=admin123 \
+  --admin-email=admin1@boxoprint.neotiq.com \
+  --admin-firstname=Admin \
+  --admin-lastname=User \
+  --backend-frontname=admin \
+  --language=fr_FR \
+  --currency=EUR \
+  --timezone=Europe/Paris \
+  --use-rewrites=1 \
+  --search-engine=elasticsearch7 --elasticsearch-host=$MAGENTO_ELASTICSEARCH_HOST --elasticsearch-port=9200
+  # Create a flag file to indicate that the setup has been completed
+  touch /var/www/html/magento-setup-done
+fi
 
 php bin/magento indexer:reindex
 php bin/magento setup:upgrade
